d654582d777398ebc6564f8b103118602a103499
Den <mr.denisyakubenko@gmail.com>
2026-01-19 20:14:56 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -34,31 +34,3 @@ class CarWashStation:
         total_score += rating
         self.count_of_ratings += 1
         self.average_rating = round(total_score / self.count_of_ratings, 1)
-
-bmw = Car(comfort_class=3, clean_mark=3, brand='BMW')
-audi = Car(comfort_class=4, clean_mark=9, brand='Audi')
-mercedes = Car(comfort_class=7, clean_mark=1, brand='Mercedes')
-
-wash_station = CarWashStation(
-    distance_from_city_center=6,
-    clean_power=8,
-    average_rating=3.9,
-    count_of_ratings=11
-)
-
-income = wash_station.serve_cars([bmw, audi, mercedes])
-
-print(f"Income: {income}")
-print(f"BMW clean mark: {bmw.clean_mark}")
-print(f"Audi clean mark: {audi.clean_mark}")
-print(f"Mercedes clean mark: {mercedes.clean_mark}")
-
-ford = Car(comfort_class=2, clean_mark=1, brand='Ford')
-wash_cost = wash_station.calculate_washing_price(ford)
-
-print(f"Cost for Ford: {wash_cost}")
-print(f"Ford clean mark: {ford.clean_mark}")
-
-wash_station.rate_service(5)
-print(f"Rating count: {wash_station.count_of_ratings}")
-print(f"Avg rating: {wash_station.average_rating}")
